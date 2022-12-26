8f5c4422d81c26919ecfbf1570f642d548db367a
choiseoeun-GoUp <79019104+choiseoeun-GoUp@users.noreply.github.com>
2022-12-26 10:31:47 +0900

    #4 Feat : 새로고침해도 데이터 유지
    - 대여페이지의 대여 데이터를 새로고침해도 유지되도록 redux-persist와 thunk를 사용해서 처리
    
    by.seoeun

--- client/src/index.js
+++ client/src/index.js
@@ -7,17 +7,24 @@ import GlobalStyle from "./assets/styles/GlobalStyle";
 import { ThemeProvider } from "styled-components";
 import theme from "./assets/styles/theme";
 import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
+import { persistStore } from "redux-persist";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
+
+let persistor = persistStore(store);
+
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter>
-        <GlobalStyle />
-        <ThemeProvider theme={theme}>
-          <App />
-        </ThemeProvider>
-      </BrowserRouter>
+      <PersistGate loading={null} persistor={persistor}>
+        <BrowserRouter>
+          <GlobalStyle />
+          <ThemeProvider theme={theme}>
+            <App />
+          </ThemeProvider>
+        </BrowserRouter>
+      </PersistGate>
     </Provider>
   </React.StrictMode>
 );
